30bd49edf7b5a9f1634fd7739157ecd86affa800
Denis O <denis@universal-development.com>
2015-06-18 11:31:29 +0300

    so far springboot is not loading web.xml

--- webapp/src/main/java/com/unidev/templatewebapp/Application.java
+++ webapp/src/main/java/com/unidev/templatewebapp/Application.java
@@ -4,22 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class Application  extends SpringBootServletInitializer{
+@SpringBootApplication
+@ImportResource(value = {"classpath:/*-beans.xml"})
+public class Application extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        // Customize the application or call application.sources(...) to add sources
-        // Since our example is itself a @Configuration class we actually don't
-        // need to override this method.
-        return application;
+        return application.sources(Application.class);
     }
 
     public static void main(String[] args) throws Exception {
